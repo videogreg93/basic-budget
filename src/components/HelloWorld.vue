462844717c8ef27d235e8299d15326ebf35e6d61
Gregory fournier <gregory.fournier@protonmail.com>
2021-03-13 18:00:03 -0500

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,133 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <form enctype="multipart/form-data">
+      <input type="file" @change="onFileChange" />
+    </form>
+    <v-chart class="chart" :option="option" />
+    <b-table striped hover :items="expenses" :fields="fields"></b-table>
   </div>
 </template>
 
 <script>
+import { use } from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { PieChart } from "echarts/charts";
+import {
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+} from "echarts/components";
+import VChart, { THEME_KEY } from "vue-echarts";
+import csv from "jquery-csv";
+
+use([
+  CanvasRenderer,
+  PieChart,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+]);
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  components: {
+    VChart,
+  },
+  provide: {
+    [THEME_KEY]: "light",
+  },
+  methods: {
+    onFileChange(e) {
+      var vue = this;
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        // We're filtering for only 2021 because of bad data before
+        var objects = csv.toObjects(e.target.result).filter(element => element.Date.split("-")[0] == 2021);
+        vue.expenses = objects;
+        vue.fields = csv.toArrays(e.target.result)[0].map(vue.toField);
+        vue.categories = new Set(objects.map(vue.mapToCategories));
+        vue.option.legend.data = Array.from(vue.categories);
+        // For each category, count up the costs
+        var costs = [];
+        this.categories.forEach((category) => {
+          var cost = objects
+            .filter((element) => element.Category == category)
+            .reduce((acc, val) => acc + parseFloat(val.Cost), 0);
+          var item = {};
+          item.value = cost;
+          item.name = category;
+          costs.push(item);
+        });
+        vue.option.series[0].data = costs;
+      };
+      reader.readAsText(file);
+    },
+    mapToCategories(item) {
+      return item.Category;
+    },
+    toField(item) {
+      var field = {}
+      field.key = item;
+      field.sortable = true;
+      return field;
+    }
+  },
+  data() {
+    return {
+      option: {
+        title: {
+          text: "Basic Budget",
+          left: "center",
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)",
+        },
+        legend: {
+          orient: "vertical",
+          left: "left",
+          data: [
+            "Direct",
+            "Email",
+            "Ad Networks",
+            "Video Ads",
+            "Search Engines",
+          ],
+        },
+        series: [
+          {
+            name: "All Time",
+            type: "pie",
+            radius: "55%",
+            center: ["50%", "60%"],
+            data: [],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: "rgba(0, 0, 0, 0.5)",
+              },
+            },
+          },
+        ],
+      },
+      categories: [],
+      expenses: [],
+      items: [],
+      fields : []
+    };
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.container {
+  display: flex;
+  flex-flow: column wrap;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.chart {
+  min-height: 400px;
+  width: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,39 @@
-import Vue from 'vue'
 import App from './App.vue'
+import Vue from 'vue'
+import ECharts from 'vue-echarts'
+import { use } from 'echarts/core'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import ECharts modules manually to reduce bundle size
+
+import {
+  CanvasRenderer
+} from 'echarts/renderers'
+import {
+  BarChart
+} from 'echarts/charts'
+import {
+  GridComponent,
+  TooltipComponent
+} from 'echarts/components'
 
 Vue.config.productionTip = false
+
+use([
+  CanvasRenderer,
+  BarChart,
+  GridComponent,
+  TooltipComponent,
+]);
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+// register globally (or you can do it locally)
+Vue.component('chart', ECharts)
 
 new Vue({
   render: h => h(App),
