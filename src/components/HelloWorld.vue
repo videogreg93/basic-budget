b05a86e0976ff1ebbe17084686a4f34935fdd6d9
Gregory fournier <gregory.fournier@protonmail.com>
2021-03-13 21:40:51 -0500

    Basic budget stuff is working ok

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,27 @@
 <template>
-  <div class="container">
-    <form enctype="multipart/form-data">
-      <input type="file" @change="onFileChange" />
-    </form>
-    <v-chart class="chart" :option="option" />
+  <b-container class="container">
+    <h1>Easy Budget</h1>
+    <b-alert
+      id="alert"
+      variant="success"
+      :show="showSuccess"
+      dismissible
+      fade
+      >{{ successAlertMessage }}</b-alert
+    >
+    <b-form-file
+      :state="Boolean(file1)"
+      @change="onFileChange"
+      placeholder="Choose a file or drop it here..."
+      drop-placeholder="Drop file here..."
+      accept=".csv"
+    ></b-form-file>
+    <b-row>
+      <v-chart class="chart" id="chart1" :option="option" />
+      <v-chart class="chart" id="chart2" :option="option2" />
+    </b-row>
     <b-table striped hover :items="expenses" :fields="fields"></b-table>
-  </div>
+  </b-container>
 </template>
 
 <script>
@@ -28,6 +44,11 @@ use([
   LegendComponent,
 ]);
 
+window.onresize = function () {
+  use.init(document.getElementById("chart1")).resize();
+  use.init(document.getElementById("chart2")).resize();
+};
+
 export default {
   name: "HelloWorld",
   components: {
@@ -43,7 +64,9 @@ export default {
       const reader = new FileReader();
       reader.onload = (e) => {
         // We're filtering for only 2021 because of bad data before
-        var objects = csv.toObjects(e.target.result).filter(element => element.Date.split("-")[0] == 2021);
+        var objects = csv
+          .toObjects(e.target.result)
+          .filter((element) => element.Date.split("-")[0] == 2021);
         vue.expenses = objects;
         vue.fields = csv.toArrays(e.target.result)[0].map(vue.toField);
         vue.categories = new Set(objects.map(vue.mapToCategories));
@@ -60,24 +83,86 @@ export default {
           costs.push(item);
         });
         vue.option.series[0].data = costs;
+        vue.getMonthlyCosts();
+        vue.showSuccess = 3;
+        vue.successAlertMessage = "File imported!";
       };
       reader.readAsText(file);
+    },
+    groupBy(list, keyGetter) {
+      const map = new Map();
+      list.forEach((item) => {
+        const key = keyGetter(item);
+        const collection = map.get(key);
+        if (!collection) {
+          map.set(key, [item]);
+        } else {
+          collection.push(item);
+        }
+      });
+      return map;
+    },
+    getMonthlyCosts() {
+      var vue = this;
+      // Group costs month
+      var grouped = Array.from(
+        vue.groupBy(
+          vue.expenses,
+          (expense) => expense.Date.split("-")[0] + expense.Date.split("-")[1]
+        )
+      );
+
+      // Calculate total cost of each category, grouped per month
+      vue.totalCostsPerMonth = grouped.map((expenseArray) => {
+        var item = {};
+        item.date = expenseArray[0];
+        item.groupedCosts = Array.from(vue.categories).map((category) => {
+          var groupedItem = {};
+          groupedItem.category = category;
+          groupedItem.cost = expenseArray[1]
+            .filter((expense) => expense.Category == category)
+            .reduce((acc, val) => acc + parseFloat(val.Cost), 0);
+          return groupedItem;
+        });
+        item.cost = expenseArray[1].reduce(
+          (acc, val) => acc + parseFloat(val.Cost),
+          0
+        );
+        return item;
+      });
+
+      // Calculate average cost per month, grouped by category
+      vue.averageCostPerMonth = Array.from(vue.categories).map((category) => {
+        var result = {};
+        result.name = category;
+        result.value =
+          vue.totalCostsPerMonth
+            .map((monthItem) => {
+              return monthItem.groupedCosts.find(
+                (item) => item.category == category
+              );
+            })
+            .reduce((acc, val) => acc + parseFloat(val.cost), 0) /
+          vue.totalCostsPerMonth.length;
+        return result;
+      });
+      vue.option2.series[0].data = Array.from(vue.averageCostPerMonth);
     },
     mapToCategories(item) {
       return item.Category;
     },
     toField(item) {
-      var field = {}
+      var field = {};
       field.key = item;
       field.sortable = true;
       return field;
-    }
+    },
   },
   data() {
     return {
       option: {
         title: {
-          text: "Basic Budget",
+          text: "Total Costs",
           left: "center",
         },
         tooltip: {
@@ -112,10 +197,39 @@ export default {
           },
         ],
       },
+      option2: {
+        title: {
+          text: "Monthly",
+          left: "center",
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)",
+        },
+        series: [
+          {
+            name: "Monthly",
+            type: "pie",
+            radius: "55%",
+            center: ["50%", "60%"],
+            data: [],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: "rgba(0, 0, 0, 0.5)",
+              },
+            },
+          },
+        ],
+      },
       categories: [],
       expenses: [],
       items: [],
-      fields : []
+      fields: [],
+      totalCostsPerMonth: [],
+      showSuccess: false,
+      successAlertMessage: "",
     };
   },
 };
@@ -128,6 +242,6 @@ export default {
 }
 .chart {
   min-height: 400px;
-  width: 100%;
+  width: 50%;
 }
 </style>
